18a50f604bed01cc123569889d127513a9940c65
Eyal Ella <eyal.ella@fundbox.com>
2017-12-10 22:17:09 +0200

    before adding custom templates

--- client/app/components/home/home-looks/home-looks.controller.js
+++ client/app/components/home/home-looks/home-looks.controller.js
@@ -1,9 +1,19 @@
-const looks = [{ name: 'wild' }, { name: 'new year\'s' }, { name: 'natural' }];
+import angular from 'angular';
+
+const looks = [{name: 'wild'}, {name: 'new year\'s'}, {name: 'natural'}];
 
 class HomeLooksController {
-  constructor() {
+  constructor($scope) {  'ngInject';
     this.looks = looks;
+    this.activeLook = looks[0].name;
+    this.onSlideChange = angular.bind(this, onSlideChange, $scope);
   }
+}
+
+function onSlideChange($scope, swiper) {
+  const look = looks[swiper.activeIndex];
+  this.activeLook = look ? look.name : '';
+  $scope.$apply();
 }
 
 export default HomeLooksController;
